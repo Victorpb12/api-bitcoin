809d93896cef2705d90fe9331bd14bca52599c76
victorpb12 <victorpbaumg@gmail.com>
2024-01-03 15:46:26 -0300

    criado buttons

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+
+
+import CurrentPrice from './src/components/CurrentPrice';
+import HistoryGraphic from './src/components/HistoryGraphic';
+import QuotationsList from './src/components/QuotationsList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar 
+        backgroundColor='#f50d41'
+        barStyle='dark-content'
+      />
+      <CurrentPrice />
+      <HistoryGraphic />
+      <QuotationsList />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 40: 0,
   },
 });
---
+++ src/components/CurrentPrice/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+export default function CurrentPrice() {
+  return (
+    <View style={styles.headerPrice}>
+      <Text style={styles.currentPrice}>R$ 259,696</Text>
+      <Text style={styles.textPrice}>Última cotação</Text>
+    </View>
+  );
+};
